a12c78a79fba8d13a7e42512c240a9afc73cefec
dahyunko <101400650+dahyunko@users.noreply.github.com>
2023-01-07 14:18:43 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 320 ms, Memory: 9836 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -1,70 +1,58 @@
-/*020- 수 정렬하기 2
-병합 정렬: O(nlongn)
-*/
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<algorithm>
+#include<vector>
 
 using namespace std;
-vector<int> list(1000001);//임시 배열
 
-void merge(vector<int>& A, int left, int mid, int right) {
+vector<int> list(1000001);//임시배열
 
-	int i = left, j = mid+1;
-	int k = left;//list 순서 정해줌
+void Merge(vector<int>& A, int start, int mid, int end) {
+	int i = start, j = mid + 1;
+	int k = start;
 
-	//분할된 list 합병
-	while (j <= right && i<=mid) {
-		if (A[i] < A[j]) {
-			list[k++] = A[i++];
-		}
-		else {
+	while (i <= mid && j <= end) {
+		if (A[i] > A[j]) {
 			list[k++] = A[j++];
 		}
+		else {
+			list[k++] = A[i++];
+		}
 	}
 
-	//남아있는 값 저장
-	if (i > mid) {
-		for (int t = j; t <= right; t++) list[k++] = A[t];
-	}
-	else {
-		for (int t = i; t <= mid; t++) list[k++] = A[t];
-	}
+	//남은것 삽입
+	while (j <= end) list[k++] = A[j++];
+	while (i<= mid) list[k++] = A[i++];
 
-	//list -> A로 이동
-	for (int t = left; t <= right; t++) {
+	for (int t = start; t <= end; t++) {
 		A[t] = list[t];
 	}
 }
 
-void merge_sort(vector<int> &A, int left, int right) {
-	if (left >= right) return;
-
-	//분할
-	int mid = (left + right) / 2;
-	//정복(conquer)
-	merge_sort(A, left, mid);
-	merge_sort(A, mid+1, right);
-	//병합
-	merge(A, left, mid, right);
+void MergeSort(vector<int>& A, int start, int end) {
+	if (start >= end) return;
+	
+	int mid = (start + end) / 2;
+	MergeSort(A, start, mid);
+	MergeSort(A, mid+1, end);
+	Merge(A, start, mid, end);
 }
 
 int main() {
-	//시간 초과
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
 	int N;
 
-	cin >> N;
+	cin >> N ;
 	vector<int> A(N, 0);
-	vector<int> list(N, 0);//임시 배열
 
 	for (int i = 0; i < N; i++) {
 		cin >> A[i];
 	}
 
-	merge_sort(A,0, N-1);
+	//퀵 정렬
+	MergeSort(A, 0, N - 1);
 
 	for (int i = 0; i < N; i++) {
 		cout << A[i]<<'\n';
