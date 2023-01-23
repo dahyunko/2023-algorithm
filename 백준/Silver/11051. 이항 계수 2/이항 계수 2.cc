8b10e7ae14ce32846fcb0231e2cc4d2b884b6a07
dahyunko <101400650+dahyunko@users.noreply.github.com>
2023-01-23 22:51:55 +0900

    [Silver II] Title: 이항 계수 2, Time: 4 ms, Memory: 5812 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.cc
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.cc
@@ -1,4 +1,4 @@
-/*077(11051)- 이항계수 구하기 2*/
+/*DP 이용*/
 #include <iostream>
 #include <algorithm>
 #include <string>
@@ -6,46 +6,24 @@
 
 using namespace std;
 
-int gcd(int a, int b) {
-    if (b == 0) return a;
-    return gcd(b, a % b);
-}
-
-void fac(int n, int k) {
-    long result = 1;
-    int Mod = 10007;
-    vector<int> A;
-    vector<int> B;
-
-    for (int i = 0; i < k; i++) { 
-        A.push_back(n - i); 
-    }
-    for (int i = 1; i <= k; i++) { 
-        B.push_back(i);
-    }
-
-    for (int i = 0; i < B.size(); i++) {
-        for (int j = 0; j < A.size(); j++) {
-            if (B[i] != 1) {
-                int n;
-                n = gcd(B[i], A[j]);
-                B[i] /= n;
-                A[j] /= n;
-            }
-        }
-    }
-
-    for (int j = 0; j < A.size(); j++) {
-        result = result * A[j];
-        result %= Mod;
-    }
-
-    cout << result;
-}
-
 int main() {
     int N, K;
     cin >> N >> K;
 
-    fac(N, K);
+    int D[1001][1001];
+
+    for (int i = 0; i <= N; i++) {
+        D[i][0] = 1;
+        D[i][i] = 1;
+        D[i][1] = i;
+    }
+
+    for (int i = 2; i <= N; i++) {
+        for (int j = 1; j < i; j++) {
+            D[i][j] = D[i - 1][j] + D[i - 1][j - 1];
+            D[i][j] = D[i][j] % 10007;
+        }
+    }
+
+    cout << D[N][K];
 }
