dd5cff7aec8824233f5caadf0eede30b5c61ee5b
dahyunko <101400650+dahyunko@users.noreply.github.com>
2023-02-06 21:33:38 +0900

    [Gold IV] Title: 타임머신, Time: 12 ms, Memory: 2220 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.cc
+++ 백준/Gold/11657. 타임머신/타임머신.cc
@@ -1,66 +1,64 @@
-/*059(11657)- 타임머신*/
-//벨만포드: 음수 사이클 찾는 것, 음수 weight 존재 하는 경우
-#include<limits.h>
+/*11657- 타임머신*/
+//벨만 포드: tuple, 엣지 중심으로 탐색
+#include <limits.h>
 #include <tuple>
-#include<iostream>
-#include<string>
+#include <iostream>
 #include<vector>
+#include<cmath>
 #include<algorithm>
+#include<string>
 
 using namespace std;
-typedef tuple<int, int, int> Edge;
+
+typedef tuple<int, int, int> edge;
 
 int main() {
 	int N, M;
 
 	cin >> N >> M;
+	vector<edge> edges;
+	vector<long> D(N + 1, LONG_MAX);
 
-	vector<Edge> edges;
-	vector<long> D(N + 1, LONG_MAX);//정답 배열
-
+	//엣지 생성
 	for (int i = 0; i < M; i++) {
-		int A, B, C;
-        cin >> A >> B >> C;
-		edges.push_back(Edge(A, B, C));
-		//시작점, 끝점, 가중치
+		int a, b, c;
+		cin >> a >> b >> c;
+
+		edges.push_back(make_tuple(a, b, c));
 	}
 
 	//벨만 포드
 	D[1] = 0;
-	for (int i = 1; i < N; i++) {//N-1번 반복
-		for (int j = 0; j < edges.size() ; j++) {
-			Edge medge = edges[j];
-			int start = get<0>(medge);
-			int end = get<1>(medge);
-			int time = get<2>(medge);
+	for (int i = 0; i < N - 1; i++) {
+		for (int j = 0; j < M; j++) {
+			int s = get<0>(edges[j]);
+			int e = get<1>(edges[j]);
+			int w = get<2>(edges[j]);
 
-			//최단 거리 갱신
-			if (D[start] != LONG_MAX && D[end] > D[start] + time) {
-				D[end] = D[start] + time;
+			//업데이트
+			if (D[s] != LONG_MAX && D[e] > D[s] + w) {
+				D[e] = D[s] + w;
 			}
 		}
 	}
 
 	//음수 사이클 확인
-	bool mcycle = false;
-	for (int i = 0; i < edges.size(); i++) {
-		Edge medge = edges[i];
-		int start = get<0>(medge);
-		int end = get<1>(medge);
-		int time = get<2>(medge);
+	bool isCycle = false;
+	for (int j = 0; j < M; j++) {
+		int s = get<0>(edges[j]);
+		int e = get<1>(edges[j]);
+		int w = get<2>(edges[j]);
 
-		if (D[start] != LONG_MAX && D[end] > D[start] + time) {
-			mcycle = true;
+		if (D[s] != LONG_MAX && D[e] > D[s] + w) {
+			isCycle = true;
 		}
 	}
 
-	if (!mcycle) {//음수 사이클 아닐 경우
+	if (isCycle) cout << -1;
+	else {
 		for (int i = 2; i <= N; i++) {
 			if (D[i] == LONG_MAX) cout << -1 << '\n';
 			else cout << D[i] << '\n';
 		}
-	}
-	else {
-		cout << -1 << '\n';
 	}
 }
