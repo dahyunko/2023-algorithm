cc0d8f917a6ef611e11d51f601dfeb0a08d3a04e
dahyunko <101400650+dahyunko@users.noreply.github.com>
2023-02-07 15:57:33 +0900

    [Silver I] Title: 트리 순회, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.cc
+++ 백준/Silver/1991. 트리 순회/트리 순회.cc
@@ -1,75 +1,51 @@
-/*070(1991)- 트리 순회하기*/
-//이진 트리
-//전위 순회한 결과 : ABDCEFG // (루트) (왼쪽 자식) (오른쪽 자식)
-//중위 순회한 결과 : DBAECFG // (왼쪽 자식) (루트) (오른쪽 자식)
-//후위 순회한 결과 : DBEGFCA // (왼쪽 자식) (오른쪽 자식) (루트)
-#include<iostream>
-#include<string>
+/*1991- 중위, 후위, 전위 순회*/
+#include <iostream>
 #include<vector>
+#include<cmath>
 #include<algorithm>
+#include<string>
 
 using namespace std;
 
-typedef pair<char, char> info;
+int tree[27][2]={-1};
 
-void pre_order(vector<info> tree, char v) {
-	int t = v - 'A';
-
-	cout << v;//루트 출력
-	if (tree[t].first != '.') {//왼쪽
-		pre_order(tree, tree[t].first);
-	}
-	if (tree[t].second != '.') {//오른쪽
-		pre_order(tree, tree[t].second);
-	}
+void preorder(int v) {
+	char now = 'A' + v -1 ;
+	cout << now;
+	if (tree[v][0] != 0) preorder(tree[v][0]); 
+	if (tree[v][1] != 0) preorder(tree[v][1]); 
 }
 
-void mid_order(vector<info> tree, char v) {
-	int t = v - 'A';
-
-	if (tree[t].first != '.') {
-		mid_order(tree, tree[t].first);
-	}
-	cout << v;//출력
-	if (tree[t].second != '.') {
-		mid_order(tree, tree[t].second);
-	}
+void inorder(int v) {
+	if (tree[v][0] != 0) inorder(tree[v][0]);
+	char now = 'A' + v -1;
+	cout << now;
+	if (tree[v][1] != 0) inorder(tree[v][1]);
 }
 
-void last_order(vector<info> tree, char v) {
-	int t = v - 'A';
-
-	if (tree[t].first != '.') {
-		last_order(tree, tree[t].first);
-	}
-	if (tree[t].second != '.') {
-		last_order(tree, tree[t].second);
-	}
-	cout << v;//출력
+void postorder(int v){
+	if (tree[v][0] != 0) postorder(tree[v][0]);
+	if (tree[v][1] != 0) postorder(tree[v][1]);
+	char now = 'A' + v -1;
+	cout << now;
 }
 
 int main() {
-	int N;//노드 개수
+	int N;
 
 	cin >> N;
-	vector<info> tree;
-	tree.resize(N);
 	for (int i = 0; i < N; i++) {
 		char a, b, c;
-		int t;
 		cin >> a >> b >> c;
-		t = a - 'A';
-		//first: left, second: right
-		tree[t].first = b;
-		tree[t].second = c;
+
+		int now = a - 'A' + 1;
+		if (b != '.') tree[now][0] = b - 'A' + 1;
+		if (c != '.') tree[now][1] = c - 'A' + 1;
 	}
 
-	//전위 순회
-	pre_order(tree, 'A');
+	preorder(1);
 	cout << '\n';
-	//중위 순회
-	mid_order(tree, 'A');
+	inorder(1);
 	cout << '\n';
-	//후위 순회
-	last_order(tree, 'A');
+	postorder(1);
 }
