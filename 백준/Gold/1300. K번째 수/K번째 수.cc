de2f5e2ff9036774453ec376e904c69d64724e7c
dahyunko <101400650+dahyunko@users.noreply.github.com>
2023-02-15 15:39:12 +0900

    [Gold II] Title: K번째 수, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.cc
+++ 백준/Gold/1300. K번째 수/K번째 수.cc
@@ -1,38 +1,31 @@
-/*31(1300)*/
-#include<iostream>
-#include<string>
+/*1300*/
+#include <iostream>
 #include<algorithm>
-#include<cmath>
 #include<vector>
+#include<string>
 
 using namespace std;
 
 int main() {
 	int N, K;
+	
 	cin >> N >> K;
-	
-	vector<int> A(N + 1);
+	vector<int>A(N + 1);
+
 	for (int i = 1; i <= N; i++) A[i] = i;
-	int l = 1, r = K;
-    long result =0;
-    
-	while (l <= r) {
-		int mid = (l + r) / 2;
+
+	int left = 1, right = K;
+	while (left <= right) {
 		int cnt = 0;
-	
-		//각 행의 mid와 같거나 작은 수 개수
+		int mid = (left + right) / 2;
+
 		for (int i = 1; i <= N; i++) {
 			cnt += min(N, mid / A[i]);
 		}
 
-		//cnt와 mid값 비교하면서 l,r값 조정
-		if (cnt >= K){
-            r = mid - 1;
-            result = mid;
-        }
-		else l = mid + 1;
+		if (cnt >= K) right = mid - 1;
+		else left = mid + 1;
 	}
 
-	//k번째 수
-	cout << result;
+	cout << left;
 }
