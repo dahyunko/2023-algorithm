c406101814f79745df3b87c937a8c3d77b8300c4
dahyunko <101400650+dahyunko@users.noreply.github.com>
2023-02-24 16:25:22 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,26 +1,28 @@
-/*084(1463)- 1로 만들기*/
-//동적 계획법- 피보나치 수열_Bottom-up
-//피보나치 수열: D[n] = D[n-1] + D[n-2]
-#include <iostream>
-#include <algorithm>
-#include <string>
-#include <vector>
+/*1463*/
+#include<limits.h>
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
 
 using namespace std;
 
 int main() {
-    int n;
+	int N;
+	
+	cin >> N;
+	vector<int> dp(N + 1, 0);
 
-    cin >> n;
-    vector<int> D(n + 1, 0);
+	dp[2] = 1;
+	dp[3] = 1;
+	for (int i = 4; i <= N; i++) {
+		int a = INT_MAX, b = INT_MAX, c = INT_MAX;
+		
+		if (i % 3 == 0) a = dp[i / 3] + 1;
+		if (i % 2 == 0) b = dp[i / 2] + 1;
+		c = dp[i - 1] + 1;
 
-    D[1] = 0;// 초기화
-
-    //동적 계획법- 피보나치 수열_Bottom-up
-    for (int i = 2; i <= n; i++) {
-        D[i] = D[i - 1] + 1;
-        if (i % 2 == 0) D[i] = min(D[i], D[i / 2] + 1);
-        if (i % 3 == 0) D[i] = min(D[i], D[i / 3] + 1);
-    }
-    cout << D[n];
+		dp[i] = min(a, min(b, c));
+	}
+	cout << dp[N];
 }
