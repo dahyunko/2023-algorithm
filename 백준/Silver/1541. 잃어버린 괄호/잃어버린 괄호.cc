f969b92562d8f8db67d46bc486e095ec7b4dc377
dahyunko <101400650+dahyunko@users.noreply.github.com>
2023-05-06 17:48:57 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 0 ms, Memory: 2080 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
@@ -1,44 +1,38 @@
-/*036(1541)*/
-//split이용, string sstream 선언
+/*1541*/
 #include<sstream>
 #include<iostream>
-#include<string>
 #include<vector>
+#include<string>
 #include<algorithm>
-
+#include<queue>
 using namespace std;
-//split 선언
-vector<string> split(string input, char delimiter) {
-	vector<string> result;
-	stringstream mydata(input);
+
+vector<string>split(string input, char delimiter) {
+	vector<string>result;
+	stringstream mystream(input);
 	string splitdata;
 
-	while (getline(mydata, splitdata, delimiter)) {
+	while (getline(mystream, splitdata, delimiter)) {
 		result.push_back(splitdata);
 	}
 	return result;
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
 	string sen;
 	cin >> sen;
-	vector<string> A = split(sen, '-');
-	
-	int sum = 0;
 
+	vector<string>A = split(sen, '-');
+
+	int sum = 0;
 	for (int i = 0; i < A.size(); i++) {
-		vector<string> B = split(A[i], '+');
 		int temp = 0;
+		vector<string>B = split(A[i], '+');
 		for (int j = 0; j < B.size(); j++) {
 			temp += stoi(B[j]);
 		}
 		if (i == 0) sum = temp;
 		else sum -= temp;
 	}
-
 	cout << sum;
 }
