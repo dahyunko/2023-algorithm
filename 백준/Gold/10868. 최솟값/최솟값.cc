85318b4dd8ae421369358646497d8a8ecdb9eafb
dahyunko <101400650+dahyunko@users.noreply.github.com>
2023-05-09 18:17:33 +0900

    [Gold I] Title: 최솟값, Time: 72 ms, Memory: 4260 KB -BaekjoonHub

--- 백준/Gold/10868. 최솟값/최솟값.cc
+++ 백준/Gold/10868. 최솟값/최솟값.cc
@@ -1,30 +1,29 @@
-/*072(10868)- 최솟값 찾기2*/
-//빈곳에 max값을 넣어줘야함
-#include<cmath>
-#include<limits.h>
-#include<iostream>
-#include<string>
+/*10868*/
+#include <iostream>
 #include<vector>
+#include<cmath>
 #include<algorithm>
+#include<string>
+#include<limits.h>
 
 using namespace std;
 
-void part_min(vector<long> &tree, int start, int end) {
-	long min_value = LONG_MAX;
-	while (start <= end) {
-		if (start % 2 == 1) {
-			min_value = min(min_value, tree[start]);
-			start++;
-		}
-		if (end % 2 == 0) {
-			min_value = min(min_value, tree[end]);
-			end--;
-		}
-		start /= 2;
-		end /= 2;
-	}
+long findmin(long left, long right, vector<long>& tree) {
+	long result = LONG_MAX;
 
-	cout << min_value << '\n';
+	while (left <= right) {
+		if (left % 2 == 1) {
+			result = min(result, tree[left]);
+			left++;
+		}
+		if (right % 2 == 0) {
+			result = min(result, tree[right]);
+			right--;
+		}
+		left /= 2;
+		right /= 2;
+	}
+	return result;
 }
 
 int main() {
@@ -34,31 +33,31 @@ int main() {
 	int N, M;
 	cin >> N >> M;
 
-	int length = N;
-	int tree_length = 0;
-	while (length != 0) {
-		length /= 2;
-		tree_length++;
+	int tree_dep = 0;
+	int n = N;
+	while (n != 0) {
+		tree_dep++;
+		n /= 2;
 	}
-	//최소 트리 생성
-	int tree_size = pow(2, tree_length + 1);
+
+	long tree_size = pow(2, tree_dep+1);
 	vector<long> tree(tree_size + 1, LONG_MAX);
-	int t = pow(2, tree_length);
-	for (int i = 0; i < N; i++) {
-		cin >> tree[t + i];
-	}
+	long t = tree_size / 2;
+
+	for (int i = t; i < t + N; i++) cin >> tree[i];
 	t--;
-	while (t > 0) {//최솟값 삽입
-		tree[t] = min (tree[t * 2], tree[2 * t + 1]);
+	//정렬
+	while (t > 0) {
+		tree[t] = min(tree[t * 2], tree[t * 2 + 1]);
 		t--;
 	}
 
-	//구간 최솟값
 	for (int i = 0; i < M; i++) {
 		long a, b;
 		cin >> a >> b;
-		a = pow(2, tree_length) + a - 1;
-		b = pow(2, tree_length) + b - 1;
-		part_min(tree, a, b);
+		a = pow(2, tree_dep) + a - 1;
+		b = pow(2, tree_dep) + b - 1;
+
+		cout<< findmin(a, b ,tree)<<'\n';
 	}
 }
