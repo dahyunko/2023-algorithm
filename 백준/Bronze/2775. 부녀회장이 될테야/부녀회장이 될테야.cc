3cc3372044f0484c2a3a687783ede60433e7d89e
dahyunko <101400650+dahyunko@users.noreply.github.com>
2023-05-09 23:59:08 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cc
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cc
@@ -1,34 +1,31 @@
-/*2775- 부녀회장이 될테야*/
-#include <iostream>
+/*2775*/
+#include<iostream>
 #include<vector>
-#include<cmath>
-#include<algorithm>
 #include<string>
+#include<algorithm>
+#include<queue>
+#include<cmath>
+#include<tuple>
+#include<limits.h>
 
 using namespace std;
 
-int A[15][15] = { 0 };
-
 int main() {
-	int T;
-
-	cin >> T;
+	int A[15][15] = { 0 };
 
 	for (int i = 0; i < 15; i++) {
-		A[i][0] = 0;
-		A[0][i] = i;
-	}
-
-	for (int i = 1; i < 15; i++) {
 		for (int j = 1; j < 15; j++) {
-			A[i][j] = A[i][j - 1] + A[i - 1][j];
+			if (i == 0) A[i][j] = j;
+			else A[i][j] = A[i - 1][j] + A[i][j-1];
 		}
 	}
 
-	for (int i = 0; i < T; i++) {
-		int k, n;//k층 n호
-
+	int T;
+	cin >> T;
+	for (int i = 0; i < T; i++){
+		int k, n;
 		cin >> k >> n;
+
 		cout << A[k][n] << '\n';
 	}
 }
