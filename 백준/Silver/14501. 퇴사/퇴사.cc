e361547e29f4784e378ad12ed16583d6c5a2779c
dahyunko <101400650+dahyunko@users.noreply.github.com>
2023-05-11 15:39:30 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -1,30 +1,35 @@
-/*14501- 퇴사*/
-#include <iostream>
+/*14501*/
+#include<iostream>
 #include<vector>
-#include<cmath>
-#include<algorithm>
 #include<string>
+#include<algorithm>
+#include<queue>
+#include<cmath>
+#include<tuple>
+#include<limits.h>
 
 using namespace std;
 
-typedef pair<int, int> info;
-
 int main() {
 	int N;
-
 	cin >> N;
-	vector<int> T(N + 2);
-	vector<int> P(N + 2);
-	vector<int> D(N + 2, 0);
+
+	vector<int>d(N + 2, 0);
+	vector<int> T(N+1);
+	vector<int> P(N+1);
+
 	for (int i = 1; i <= N; i++) {
 		cin >> T[i] >> P[i];
 	}
 
-	for (int i = N; i >0; i--) {
-        //중간에 있는 수들 중 N값을 넘어가는 것 존재
-		if (i + T[i] > N + 1) {
-            D[i] = D[i + 1];
-        }else D[i] = max(D[i + T[i]] + P[i], D[i + 1]);
+	for (int i = N; i > 0; i--) {
+		if (i + T[i] - 1 > N) { 
+			d[i] = d[i + 1];
+			continue; 
+		}
+
+		d[i] = max(d[i + T[i]] + P[i], d[i + 1]);
 	}
-	cout << D[1];
+
+	cout << d[1];
 }
