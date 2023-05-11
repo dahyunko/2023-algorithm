e28ab799cd57c89830400f004f9951f364097200
dahyunko <101400650+dahyunko@users.noreply.github.com>
2023-05-12 02:38:59 +0900

    [Gold I] Title: 최솟값, Time: 72 ms, Memory: 3236 KB -BaekjoonHub

--- 백준/Gold/10868. 최솟값/최솟값.cc
+++ 백준/Gold/10868. 최솟값/최솟값.cc
@@ -1,33 +1,18 @@
 /*10868*/
-#include <iostream>
+#include<iostream>
 #include<vector>
-#include<cmath>
-#include<algorithm>
 #include<string>
+#include<algorithm>
+#include<queue>
+#include<stack>
+#include<cmath>
+#include<tuple>
 #include<limits.h>
 
 using namespace std;
 
-long findmin(long left, long right, vector<long>& tree) {
-	long result = LONG_MAX;
-
-	while (left <= right) {
-		if (left % 2 == 1) {
-			result = min(result, tree[left]);
-			left++;
-		}
-		if (right % 2 == 0) {
-			result = min(result, tree[right]);
-			right--;
-		}
-		left /= 2;
-		right /= 2;
-	}
-	return result;
-}
-
 int main() {
-    ios::sync_with_stdio(false);
+	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 	int N, M;
@@ -40,24 +25,33 @@ int main() {
 		n /= 2;
 	}
 
-	long tree_size = pow(2, tree_dep+1);
-	vector<long> tree(tree_size + 1, LONG_MAX);
-	long t = tree_size / 2;
+	int tree_size = pow(2, tree_dep + 1);
+	vector<int>tree(tree_size + 1, INT_MAX);
+	int t = pow(2, tree_dep);
 
-	for (int i = t; i < t + N; i++) cin >> tree[i];
+	for (int i = 0; i < N; i++) cin >> tree[t + i];
+
 	t--;
-	//정렬
 	while (t > 0) {
 		tree[t] = min(tree[t * 2], tree[t * 2 + 1]);
 		t--;
 	}
 
 	for (int i = 0; i < M; i++) {
-		long a, b;
+		int a, b, ans = INT_MAX;
 		cin >> a >> b;
-		a = pow(2, tree_dep) + a - 1;
-		b = pow(2, tree_dep) + b - 1;
 
-		cout<< findmin(a, b ,tree)<<'\n';
+		a = a + pow(2, tree_dep) - 1;
+		b = b + pow(2, tree_dep) - 1;
+
+		while (a <= b) {
+			if (a % 2 == 1) { ans = min(ans, tree[a]); a++; }
+			if (b % 2 == 0) { ans = min(ans, tree[b]); b--; }
+
+			a /= 2;
+			b /= 2;
+		}
+
+		cout << ans << '\n';
 	}
 }
