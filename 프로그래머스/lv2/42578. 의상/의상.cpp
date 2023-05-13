760ecd6895c28d5936f1667cb14aff070f7c1725
dahyunko <101400650+dahyunko@users.noreply.github.com>
2023-05-13 16:31:14 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.cpp
+++ 프로그래머스/lv2/42578. 의상/의상.cpp
@@ -9,20 +9,13 @@ using namespace std;
 int solution(vector<vector<string>> clothes) {
     int answer = 0;
     
-    vector<string>types;
     map<string, int> map;
     
-    for(int i=0;i<clothes.size();i++){
-        if(find(types.begin(), types.end(), clothes[i][1]) == types.end()){
-            types.push_back(clothes[i][1]);
-            map[clothes[i][1]] = 0;
-        }
-        map[clothes[i][1]]++;
-    }
+    for(int i=0;i<clothes.size();i++)map[clothes[i][1]]++;
     
     int ans=1;
-    for(int i=0;i<types.size();i++){
-        ans *= map[types[i]]+1;
+    for(auto it : map){
+        ans *= it.second+1;
         //cout<<map[types[i]]<<'\n';
     }
     answer = ans -1;
