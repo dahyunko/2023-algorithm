2c272f20749161ffd8075d429beaf8b39f24ebc5
dahyunko <101400650+dahyunko@users.noreply.github.com>
2023-06-18 18:24:04 +0900

    [level 2] Title: 오픈채팅방, Time: 135.45 ms, Memory: 43.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+map<string, string> m;
+
+vector<string> split(string input, char delimiter){
+    vector<string> result;
+    stringstream ss(input); 
+    string sen;
+    
+    while(getline(ss, sen, delimiter)) result.push_back(sen);
+    return result;
+}
+
+typedef pair<string, bool> EL;
+vector<string> solution(vector<string> record) {
+    vector<string> answer;
+    vector<EL> line;
+    
+    for(int t=0;t<record.size();t++){
+        vector<string> info = split(record[t],' ');
+        
+        if(info[0][0] =='E'){//들어온것
+            m[info[1]] = info[2];
+            line.push_back({info[1],true});
+            //answer.push_back(info[2]+"님이 들어왔습니다.");
+        }
+        else if(info[0][0] =='L'){//나간것
+            line.push_back({info[1],false});
+            //answer.push_back(m[info[1]]+"님이 나갔습니다.");
+        }
+        else if(info[0][0] =='C'){//바꾼것
+            m[info[1]] = info[2];
+        }
+    }
+    
+    for(int i=0;i<line.size();i++){
+        string id = line[i].first;
+        if(line[i].second)answer.push_back(m[id]+"님이 들어왔습니다.");
+        else answer.push_back(m[id]+"님이 나갔습니다.");
+    }
+    
+    return answer;
+}
