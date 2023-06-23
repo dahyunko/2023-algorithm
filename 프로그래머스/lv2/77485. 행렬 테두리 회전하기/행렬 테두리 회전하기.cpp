52b648f946eb93198060028a53e4703d28f2bcc0
dahyunko <101400650+dahyunko@users.noreply.github.com>
2023-06-23 21:53:06 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 2.35 ms, Memory: 5.94 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.cpp
@@ -0,0 +1,59 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int rows, int columns, vector<vector<int>> queries) {
+    vector<int> answer;
+    
+    //테이블 생성
+    int tab[101][101];
+    int c=1;
+    for(int i=1;i<=rows;i++){
+        for(int j=1;j<=columns;j++) tab[i][j] = c++;
+    }
+    
+    for(int q=0;q < queries.size();q++){
+        int ans = rows * columns;
+        int x1 = queries[q][0], x2 = queries[q][1], y1= queries[q][2], y2 = queries[q][3];
+        int temp;
+        
+        //위
+        temp = tab[x1][x2];
+        for(int j= x2;j< y2;j++){
+            int tmp;
+            tmp = tab[x1][j+1];
+            tab[x1][j+1] = temp;
+            temp = tmp;
+            ans = min(temp, ans);
+        }
+        //오른쪽
+        for(int i= x1;i<y1;i++){
+            int tmp;
+            tmp = tab[i+1][y2];
+            tab[i+1][y2] = temp;
+            temp = tmp;
+            ans = min(temp, ans);
+        }
+        //아래
+        for(int j = y2; j> x2;j--){
+            int tmp;
+            tmp = tab[y1][j-1];
+            tab[y1][j-1] = temp;
+            temp = tmp;
+            ans = min(temp, ans);
+        }
+        //왼쪽
+        for(int i= y1;i>x1;i--){
+            int tmp;
+            tmp = tab[i-1][x2];
+            tab[i-1][x2] = temp;
+            temp = tmp;
+            ans = min(temp, ans);
+        }
+        
+        answer.push_back(ans);
+    }
+    
+    return answer;
+}
