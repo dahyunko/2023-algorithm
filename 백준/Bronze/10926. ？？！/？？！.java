5755cceab303453e7bd4eb9146d41a65e534576b
dahyunko <101400650+dahyunko@users.noreply.github.com>
2023-07-01 14:43:13 +0900

    [Bronze V] Title: ??!, Time: 212 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		String name;
+		
+		Scanner sc = new Scanner(System.in);
+		name = sc.next();
+		
+		System.out.println(name+"??!");
+	}
+}
