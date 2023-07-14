a5f22969a775d664e73dc376043f026aec96745e
dahyunko <101400650+dahyunko@users.noreply.github.com>
2023-07-14 16:14:53 +0900

    [Gold V] Title: 틱택토, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/7682. 틱택토/틱택토.cc
+++ 백준/Gold/7682. 틱택토/틱택토.cc
@@ -12,8 +12,9 @@ using namespace std;
 
 char A[3][3];
 
-bool find_if_there_is_line(char c) {
+bool it_has(char c) {
 	int cnt = 0;
+
 	if (A[0][0] == A[0][1] && A[0][1] == A[0][2] && A[0][0] == c) cnt++;
 	if (A[1][0] == A[1][1] && A[1][1] == A[1][2] && A[1][0] == c) cnt++;
 	if (A[2][0] == A[2][1] && A[2][1] == A[2][2] && A[2][0] == c) cnt++;
@@ -22,35 +23,32 @@ bool find_if_there_is_line(char c) {
 	if (A[0][0] == A[1][0] && A[1][0] == A[2][0] && A[0][0] == c) cnt++;
 	if (A[0][1] == A[1][1] && A[1][1] == A[2][1] && A[0][1] == c) cnt++;
 	if (A[0][2] == A[1][2] && A[1][2] == A[2][2] && A[0][2] == c) cnt++;
-
-	if (cnt > 0) return true;
-	return false;
+	
+	if(cnt == 0)return false;
+	return true;
 }
 
 int main() {
-	
-	while(true) {
-		string now;
-		cin >> now;
+	while (true) {
+		string s;
+		cin >> s;
+		if (s == "end") break;
 
-		if (now == "end") break;
-		
-		//판 생성
-		int t = 0, x_cnt =0, o_cnt= 0;
+		int t = 0, x_cnt =0, o_cnt = 0;
 		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				if (now[t] == 'O') o_cnt++;
-				else if(now[t] == 'X') x_cnt++;
-				A[i][j] = now[t++];
+			for (int j = 0; j < 3; j++) { 
+				if (s[t] == 'X') x_cnt++;
+				else if (s[t] == 'O') o_cnt++;
+				A[i][j] = s[t++]; 
 			}
 		}
-		
-		bool o_has = find_if_there_is_line('O');
-		bool x_has = find_if_there_is_line('X');
 
-		if (!o_has && !x_has && x_cnt == 5 && o_cnt == 4) cout << "valid\n";
-		else if (x_has && !o_has && x_cnt == o_cnt + 1) cout << "valid\n";
-		else if (!x_has && o_has && x_cnt == o_cnt) cout << "valid\n";
+		bool x_has = it_has('X');
+		bool o_has = it_has('O');
+
+		if (!x_has && !o_has && x_cnt == 5 && o_cnt == 4) cout << "valid\n";
+		else if (x_has && !o_has && x_cnt == o_cnt + 1)cout << "valid\n";
+		else if (!x_has && o_has && x_cnt == o_cnt)cout << "valid\n";
 		else cout << "invalid\n";
 	}
 }
