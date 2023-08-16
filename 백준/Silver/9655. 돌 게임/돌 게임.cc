231e646ba71a7c547417162008854ea51ec03198
dahyunko <101400650+dahyunko@users.noreply.github.com>
2023-08-16 17:35:56 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,20 @@
+/*9655*/
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include <queue>
+#include <cmath>
+#include<limits.h>
+#include<set>
+#include<map>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+
+	if (N % 2 == 0) cout << "CY";
+	else cout << "SK";
+}
