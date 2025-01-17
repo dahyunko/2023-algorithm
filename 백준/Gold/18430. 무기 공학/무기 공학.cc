35f99ff8ace1359c5bb9e47ab817a8a989a4dba2
dahyunko <101400650+dahyunko@users.noreply.github.com>
2025-01-17 16:36:26 +0900

    [Gold IV] Title: 무기 공학, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/18430. 무기 공학/무기 공학.cc
+++ 백준/Gold/18430. 무기 공학/무기 공학.cc
@@ -1,4 +1,3 @@
-/*18430*/
 #include <iostream>
 #include <algorithm>
 #include <queue>
@@ -15,46 +14,46 @@ bool visited[6][6] = { false };
 int a[4] = { -1, 0, 1, 0 };
 int b[4] = { 0, 1, 0, -1 };
 
-bool isTrue(int y, int x) {
-	return (y >= 0 && y < N && x >= 0 && x < M);
+bool isTrue(int x, int y) {
+	return (x >= 0 && x < N && y >= 0 && y < M);
 }
 
-int checkBoard(int y, int x) {
-	int result = 0;
+int checkBoard(int x, int y) {
+	int answer = 0;
 
 	for (int t = 0; t < 4; t++) {
-		int y1 = y + a[t % 4];
-		int x1 = x + b[t % 4];
-		int y2 = y + a[(t + 1) % 4];
-		int x2 = x + b[(t + 1) % 4];
+		int x1 = x + a[t % 4];
+		int y1 = y + b[t % 4];
+		int x2 = x + a[(t + 1) % 4];
+		int y2 = y + b[(t + 1) % 4];
 
 		// 부메랑 만들 수 있는지 확인
-		if (!isTrue(y1, x1) || !isTrue(y2, x2)) continue;
-		if (visited[y1][x1] || visited[y2][x2]) continue;
+		if (!isTrue(x1, y1) || !isTrue(x2, y2)) continue;
+		if (visited[x1][y1] || visited[x2][y2]) continue;
 
-		visited[y][x] = true;
-		visited[y1][x1] = true;
-		visited[y2][x2] = true;
-		int sum = board[y][x] * 2 + board[y1][x1] + board[y2][x2];
+		visited[x1][y1] = true;
+		visited[x2][y2] = true;
+		visited[x][y] = true;
+		int sum = board[x][y] * 2 + board[x1][y1] + board[x2][y2];
 
 		// 이동할 수 있는 조합 이동
 		int temp = 0;
-		int c = x + 1;
-		for (int i = y; i < N; i++) {
+		int c = y + 1;
+		for (int i = x; i < N; i++) {
 			for (int j = c; j < M; j++) {
 				if (visited[i][j]) continue;
 				temp = max(temp, checkBoard(i, j));
 			}
 			c = 0;
 		}
-		visited[y1][x1] = false;
-		visited[y2][x2] = false;
-		visited[y][x] = false;
+		visited[x1][y1] = false;
+		visited[x2][y2] = false;
+		visited[x][y] = false;
 
-		result = max(result, temp + sum);
+		answer = max(answer, temp + sum);
 	}
-	
-	return result;
+
+	return answer;
 }
 
 int main() {
@@ -65,7 +64,7 @@ int main() {
 
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
-			result = max(checkBoard(i, j), result);
+			result = max(result, checkBoard(i, j));
 		}
 	}
 
