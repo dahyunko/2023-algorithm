bfea36a8d3966d3bc655525a46ab70ea1cce92c8
dahyun Ko <101400650+dahyunko@users.noreply.github.com>
2025-11-06 22:05:20 +0900

    [Gold III] Title: 개미굴, Time: 176 ms, Memory: 18876 KB -BaekjoonHub

---
+++ 백준/Gold/14725. 개미굴/개미굴.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+  static int N;
+  static List<String> strs = new ArrayList<>();
+  static List<String> words = new ArrayList<>();
+  static List<List<Integer>> graph = new ArrayList<>();
+  static boolean[] visited;
+
+  static void dfs(int v, int depth) {
+    if(visited[v]) return;
+    visited[v] = true;
+
+    if(depth > 0) {
+      String str = "";
+      for(int i=0;i<depth-1;i++) str += "--";
+      str += words.get(v);
+      System.out.println(str);
+    }
+
+    for(int next : graph.get(v)) {
+      dfs(next, depth+1);
+    }
+  }
+
+  public static void main(String[] args) throws Exception{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    N = Integer.parseInt(br.readLine());
+    
+    for(int i=0;i<N;i++) {
+      st = new StringTokenizer(br.readLine());    
+
+      int M = Integer.parseInt(st.nextToken());
+      String str = "";
+      for(int j=0;j<M;j++) {
+        str += st.nextToken() + " ";
+      }
+
+      strs.add(str);
+    }
+
+    Collections.sort(strs);
+
+    words.add("default");
+    graph.add(new ArrayList<>());
+
+    for(int i=0;i<strs.size();i++) {
+      String[] list = strs.get(i).split(" ");
+      int idx = 0;
+
+      for(int j=0;j<list.length;j++) {
+        int next = -1;
+
+        for(int v=0;v<graph.get(idx).size();v++) {
+          int node = graph.get(idx).get(v);
+          if(words.get(node).equals(list[j])) {
+            next = node;
+            break;
+          }
+        }
+
+        if(next == -1) {
+          words.add(list[j]);
+          next = words.size() - 1;
+          graph.get(idx).add(next);
+          graph.add(new ArrayList<>());
+        }
+        idx = next;
+      }
+    }
+
+    // print
+    visited = new boolean[words.size()];
+    dfs(0, 0);
+  }
+}
